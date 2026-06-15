2883ef7966f602dc3d450bd4f480f6dcd09d609d
Yusuke Kawamoto <novogrammer@gmail.com>
2026-06-15 19:11:46 +0900

    getting-started/with-html-svg

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,112 @@
 import './style.css'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<div>Hello</div>
-`
+import core from "@theatre/core"
+import studioRaw from "@theatre/studio"
+
+// (window as any).__TheatreJS_CoreBundle = core;
+
+console.log(core);
+console.log(studioRaw);
+const studio:typeof studioRaw = (studioRaw as any).default || studioRaw;
+studio.initialize();
+
+
+// Exported by clicking the project name and "Export Project Name to JSON" button.
+const projectState = {
+  sheetsById: {
+    'Sheet 1': {
+      staticOverrides: {
+        byObject: {
+          'Heading 1': {
+            opacity: 0.9936708860759493,
+          },
+        },
+      },
+      sequence: {
+        subUnitsPerUnit: 30,
+        length: 10,
+        type: 'PositionalSequence',
+        tracksByObject: {
+          'Heading 1': {
+            trackData: {
+              LrJ3eujCAE: {
+                type: 'BasicKeyframedTrack',
+                keyframes: [
+                  {
+                    id: 'i-s2GT5JFm',
+                    position: 0,
+                    connectedRight: true,
+                    handles: [0.5, 1, 0.882, 0],
+                    value: 0,
+                  },
+                  {
+                    id: 'I3Suv35jmV',
+                    position: 3.433,
+                    connectedRight: true,
+                    handles: [0.055, 0.969, 0.82, -0.031],
+                    value: 96,
+                  },
+                  {
+                    id: 'M_00uVAeuK',
+                    position: 5.6,
+                    connectedRight: true,
+                    handles: [0.087, 1.01, 0.5, 0],
+                    value: 0,
+                  },
+                ],
+              },
+              '9JzZUrUSb7': {
+                type: 'BasicKeyframedTrack',
+                __debugName: 'Heading 1:["opacity"]',
+                keyframes: [
+                  {
+                    id: 'EfAPcjrYAy',
+                    position: 0,
+                    connectedRight: true,
+                    handles: [0.5, 1, 0.5, 0],
+                    value: 0.9936708860759493,
+                  },
+                  {
+                    id: 'hZ-tUkMP4C',
+                    position: 2.367,
+                    connectedRight: true,
+                    handles: [0.5, 1, 0.5, 0],
+                    value: 0,
+                  },
+                  {
+                    id: 'D5PA_XGfS6',
+                    position: 5.6,
+                    connectedRight: true,
+                    handles: [0.5, 1, 0.5, 0],
+                    value: 0.9936708860759493,
+                  },
+                ],
+              },
+            },
+            trackIdByPropPath: {
+              '["y"]': 'LrJ3eujCAE',
+              '["opacity"]': '9JzZUrUSb7',
+            },
+          },
+        },
+      },
+    },
+  },
+  definitionVersion: '0.4.0',
+  revisionHistory: ['vLg01lxRrpP8eGsS'],
+}
+
+const project = core.getProject('HTML Animation Tutorial',{state:projectState});
+const sheet = project.sheet('Sheet 1');
+const obj = sheet.object('Heading 1', {
+  y: 0, // you can use just a simple default value
+  opacity: core.types.number(1, { range: [0, 1] }), // or use a type constructor to customize
+});
+
+const articleHeadingElement = document.getElementById('article-heading')!;
+
+obj.onValuesChange((obj) => {
+  articleHeadingElement.style.transform = `translateY(${obj.y}px)`;
+  articleHeadingElement.style.opacity = `${obj.opacity}`;
+});
 
